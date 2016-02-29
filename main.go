ff872c6055fc1f6775744794b2fe7820d9079d7b
Michelle Noorali <michellemolu@gmail.com>
2016-02-29 16:25:38 -0700

    feat(*): add rules command and subcommands

--- main.go
+++ main.go
@@ -25,10 +25,52 @@ func main() {
 				lint(path)
 			},
 		},
+		{
+			Name:  "rules",
+			Usage: "options for chart rules",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new chart linting rule",
+					Action: func(c *cli.Context) {
+						path := c.Args().First()
+						addRules(path)
+					},
+				},
+				{
+					Name:  "list",
+					Usage: "list chart rules",
+					Action: func(c *cli.Context) {
+						listRules(c)
+					},
+				},
+				{
+					Name:    "remove",
+					Aliases: []string{"rm"},
+					Usage:   "remove a chart rule",
+					Action: func(c *cli.Context) {
+						path := c.Args().First()
+						removeRule(path)
+					},
+				},
+			},
+		},
 	}
 
 }
 
 func lint(path string) {
+	fmt.Println("coming soon")
+}
+
+func addRules(path string) {
+	fmt.Println("coming soon")
+}
+
+func listRules(c *cli.Context) {
+	fmt.Println("coming soon")
+}
+
+func removeRule(path string) {
 	fmt.Println("coming soon")
 }
